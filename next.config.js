9100d7a50e996770eade7c5c177425bec0393b64
Luca Meneghini <lucamene98@outlook.it>
2021-09-22 15:52:51 +0200

    Add assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: '/porfolio-v2/'
+  assetPrefix: '/porfolio-v2/',
+  basePath: '/porfolio-v2/'
 }
